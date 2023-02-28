a4f141db2328ddc4a1299106a0a1a3cde531f2bd
LAPTOP-3G1BG9JP\joshb <joshbello25@gmail.com>
2023-02-28 12:01:20 +0000

    modified the Track class. Modified Track.add and the constructor class

--- cwk1/src/main/java/Track.java
+++ cwk1/src/main/java/Track.java
@@ -4,21 +4,39 @@
  * @author YOUR NAME
  */
 
+import java.util.ArrayList;
+import java.util.List;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Track {
-  public Track() {
+  
+  private List<Point> listOfPoints;
+  
+  public Track(String points, List<Point> listOfPoints){
+    this.listOfPoints = listOfPoints;
 
   }
 
-  public static void readFile(String filename) {
-
+  public static void readFile(String filename) throws IOException{
+    try {
+      File csvFile = new File(filename);
+      Scanner lines = new Scanner(csvFile);
+      
+      while (lines.hasNextLine()) {
+        String line = lines.nextLine();
+      }
+      lines.close();
+    } catch (FileNotFoundException e) {
+      throw new IOException("Error: could not access file", e)
+    }
   }
 
   public static void add(Point point) {
-
+    List<Point> pointsList = new ArrayList<Point>();
+    pointsList.add(point);
   }
 
   public Point get(int index) {

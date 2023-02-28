eb21ff9b782facc4c805ace4679785f7631ef437
LAPTOP-3G1BG9JP\joshb <joshbello25@gmail.com>
2023-02-28 17:52:46 +0000

    modifyed add, readfile, get and size methods

--- cwk1/src/main/java/Track.java
+++ cwk1/src/main/java/Track.java
@@ -20,35 +20,52 @@ public class Track {
     this.listOfPoints = new ArrayList<Point>();
   }
 
-  public void readFile(String filename) throws IOException{
+  public void readFile(String filename) throws IOException {
     try {
-      File csvFile = new File(filename);
-      Scanner lines = new Scanner(csvFile);
-      
-      while (lines.hasNextLine()) {
-        String line = lines.nextLine();
+        File csvFile = new File(filename);
+        Scanner lines = new Scanner(csvFile);
+        int lineNumber = 0;
 
-        if (!line.isEmpty()) {
-          Scanner lineScanner = new Scanner(line);
-          lineScanner.useDelimiter(",");
+        while (lines.hasNextLine()) {
+          //creates a string of each line  
+          String line = lines.nextLine();
+            
+            //checking number of values on the line
+            String[] elements = line.split(",");
+            if (elements.length != 4) {
+                System.out.println("Error: Line " + lineNumber + " has less than 4 elements");
+                continue;
+            }
+            //increments the line number
+            lineNumber++;
 
-          ZonedDateTime readTime = ZonedDateTime.parse(lineScanner.next());
-          double readLon = Double.parseDouble(lineScanner.next());
-          double readLat = Double.parseDouble(lineScanner.next());
-          double readEla = Double.parseDouble(lineScanner.next());
+            // checks the line number
+            // if lineNumber is greater than 1 means counting from line 1 (disregards headers)
+            // also checks if the line is not empty
+            if (lineNumber > 1 && !line.isEmpty()) { 
+                Scanner lineScanner = new Scanner(line);
+                lineScanner.useDelimiter(",");
 
-          Point readPoint = new Point(readTime, readLon, readLat, readEla);
-          listOfPoints.add(readPoint);
-          lineScanner.close();
+                // parse at each comma, then create variable for each element
+                ZonedDateTime readTime = ZonedDateTime.parse(lineScanner.next());
+                double readLon = Double.parseDouble(lineScanner.next());
+                double readLat = Double.parseDouble(lineScanner.next());
+                double readEla = Double.parseDouble(lineScanner.next());
+
+                // create the point variable for this line then add to the List
+                Point readPoint = new Point(readTime, readLon, readLat, readEla);
+                listOfPoints.add(readPoint);
+                lineScanner.close();
+            }
         }
-
-      }
-      lines.close();
-
-    } catch (FileNotFoundException e) {
-      throw new IOException("Error: could not access file", e);
+        lines.close();
+    //exception for invalid file
+    } catch (IOException e) {
+        System.out.println("Invalid File");
+        throw e;
     }
-  }
+}
+  
 
   public void add(Point point) {
     listOfPoints.add(point);
@@ -57,15 +74,15 @@ public class Track {
   public Point get(int index) {
     try {
         return listOfPoints.get(index);
-    } catch  {
-        System.out.println("Error: " + e.getMessage());
-        return null;
+    } catch (IndexOutOfBoundsException e) {
+        throw new GPSException("Error: " + e.getMessage());
     }
 }
 
-
   public int size() {
-    return 0;
+    int sizeOfTrack = listOfPoints.size();
+    return sizeOfTrack;
+    
   }
 
   public Point lowestPoint() {
